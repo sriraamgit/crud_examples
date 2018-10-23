59635239096440694c2c86225b573e24ec4e09e8
sriraamgit <srivm2008gmail.com>
2018-10-23 11:37:15 +0530

    Initial commit

---
+++ example_get.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+languages = [
+    {'name':'javascript'},
+    {'name':'python'},
+    {'name':'ruby'}
+]
+@app.route('/',methods = ["GET"])
+def test():
+    return jsonify({'message' : 'it works'})
+@app.route('/lang', methods = ["GET"])
+def returnAll():
+    return jsonify({'languages': languages})
+@app.route('/lang/<string:name>', methods =["GET"])
+def returnOne(name):
+    langs = [language for language in languages if language['name']==name]
+    return jsonify({'language':langs[0]})
+if __name__ =='__main__':
+    app.run(debug=True,port=8080)
---
+++ example_post.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+languages = [
+    {'name':'javascript'},
+    {'name':'python'},
+    {'name':'ruby'}
+]
+@app.route('/',methods = ["GET"])
+def test():
+    return jsonify({'message' : 'it works'})
+@app.route('/lang', methods = ["GET"])
+def returnAll():
+    return jsonify({'languages': languages})
+@app.route('/lang/<string:name>', methods =["GET"])
+def returnOne(name):
+    langs = [language for language in languages if language['name']==name]
+    return jsonify({'language':langs[0]})
+@app.route('/lang', methods = ["POST"])
+def addOne():
+    language = {'name':request.json['name']}
+    languages.append(languages)
+    return jsonify({'languages':languages})
+if __name__ =='__main__':
+    app.run(debug=True,port=8080)
