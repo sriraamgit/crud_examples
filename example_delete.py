0ca1b62f3f23cdfe560d7b8cd33a5acc6490b54f
sriraamgit <42667452+sriraamgit@users.noreply.github.com>
2018-10-24 11:58:17 +0530

    Update example_delete.py

--- example_delete.py
+++ example_delete.py
@@ -18,11 +18,12 @@ def returnOne(name):
 @app.route('/lang', methods=["POST"])
 def addOne():
     language = {'name':request.json['name']}
-    languages.append(languages)
+    languages.append(language)
     return jsonify({'languages':languages})
 @app.route('/lang/<string:name>', methods = ['PUT'])
 def editOne(name):
     langs = [language for language in languages if language['name']==name]
+    lang[0]['name'] = request.json['name']
     return jsonify({'language': langs[0]})
 @app.route('/lang/<string:name>', methods = ["DELETE"])
 def removeOne(name):
